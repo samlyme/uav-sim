ddbe143aa8c04d5235aee0ce71effbe52831ff73
samlyme <sam@samly.me>
2025-08-20 15:11:07 -0700

    working quat kinematics

--- main.py
+++ main.py
@@ -1,100 +1,105 @@
 import asyncio
 import json
 import sys
+from typing import Annotated
 import numpy as np
+import numpy.typing as npt
 import websockets
 import tty
 import termios
 
-Num = np.float32
+Num = np.float64
+EulerAngle = Annotated[npt.NDArray[Num], (3,)]
+Vector3 = Annotated[npt.NDArray[Num], (3,)]
+Quaternion = Annotated[npt.NDArray[Num], (4,)]
+RotationMatrix = Annotated[npt.NDArray[Num], (3, 3)]
 
+def e_to_q(e: EulerAngle) -> Quaternion:
+    """
+    Convert Euler angles to quaternion.
+    This is a placeholder function; actual implementation may vary.
+    """
+    phi, theta, psi = e[0], e[1], e[2]
+    cy = np.cos(psi * 0.5)
+    sy = np.sin(psi * 0.5)
+    cp = np.cos(theta * 0.5)
+    sp = np.sin(theta * 0.5)
+    cr = np.cos(phi * 0.5)
+    sr = np.sin(phi * 0.5)
+
+    q = np.array([
+        cr * cp * cy + sr * sp * sy,
+        sr * cp * cy - cr * sp * sy,
+        cr * sp * cy + sr * cp * sy,
+        cr * cp * sy - sr * sp * cy
+    ], dtype=Num)
+
+    return q / np.linalg.norm(q)
+
+def qm(a: Quaternion, b: Quaternion) -> Quaternion:
+    """
+    Quaternion multiplication.
+    """
+    w1, x1, y1, z1 = a
+    w2, x2, y2, z2 = b
+    return np.array([
+        w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2,
+        w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2,
+        w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2,
+        w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+    ], dtype=Num)
+
+def q_to_R(q: Quaternion) -> RotationMatrix:
+    """
+    Convert quaternion to rotation matrix.
+    """
+    w, x, y, z = q
+    return np.array([
+        [1 - 2 * (y**2 + z**2), 2 * (x * y - w * z), 2 * (x * z + w * y)],
+        [2 * (x * y + w * z), 1 - 2 * (x**2 + z**2), 2 * (y * z - w * x)],
+        [2 * (x * z - w * y), 2 * (y * z + w * x), 1 - 2 * (x**2 + y**2)]
+    ], dtype=Num)
+    
 LINEAR_SPEED = 1
 ANGULAR_SPEED = np.pi / 4
 
 
-def R_b_v(e: np.ndarray) -> np.ndarray:
-    """
-    Rotation matrix from body frame to inertial frame using Euler angles.
-    """
-    phi, theta, psi = e[0], e[1], e[2]  
-    R = np.array(
-        [
-            [
-                np.cos(theta) * np.cos(psi),
-                np.sin(theta) * np.sin(phi) * np.cos(psi) - np.cos(phi) * np.sin(psi),
-                np.cos(phi) * np.sin(theta) * np.cos(psi) + np.sin(phi) * np.sin(psi),
-            ],
-            [
-                np.cos(theta) * np.sin(psi),
-                np.sin(phi) * np.sin(theta) * np.sin(psi) + np.cos(phi) * np.cos(psi),
-                np.cos(phi) * np.sin(theta) * np.sin(psi) - np.sin(phi) * np.cos(psi),
-            ],
-            [
-                -np.sin(theta),
-                np.sin(phi) * np.cos(theta),
-                np.cos(phi) * np.cos(theta),
-            ]
-        ],
-        dtype=Num,
-    )
-    return R
-
-
-def J_b_v(e: np.ndarray) -> np.ndarray:
-    """
-    Jacobian of the Euler angles with respect to the angular velocity in body frame.
-    This is a 3x3 matrix that relates the angular velocity in body frame to the time derivative of the Euler angles.
-    """
-    phi, theta, psi = e[0], e[1], e[2]  # noqa: F841
-    return np.array(
-        [
-            [1, np.sin(phi) * np.tan(theta), np.cos(phi) * np.tan(theta)],
-            [0, np.cos(phi), -np.sin(phi)],
-            [0, np.sin(phi) / np.cos(theta), np.cos(phi) / np.cos(theta)],
-        ],
-        dtype=Num,
-    )
-
-
-class Drone:
-    def __init__(self, P_i_0: np.ndarray, e_0: np.ndarray):
+class KinematicObject:
+    def __init__(self, P_i_0: Vector3, q_i_0: Quaternion):
         self.P_i = P_i_0  # Position in inertial frame (pn, pe, -h)
-        self.e = e_0  # Euler angles (phi, theta, eta)
+        self.q_i = q_i_0
 
-    def update(self, v_b: np.ndarray, omega_b: np.ndarray, dt: Num):
-        J = J_b_v(self.e)
-        de = J.T @ omega_b
-        self.e = self.e + de * dt
+    def update(self, v_b: Vector3, omega_b: EulerAngle, dt: Num):
+        R = q_to_R(self.q_i)  # Rotation matrix from inertial to body frame
+        v_i = R @ v_b  
+        self.P_i += v_i * dt
 
-        R = R_b_v(self.e)
-        dp = R @ v_b
-        self.P_i = self.P_i + dp * dt
+        dq = e_to_q(omega_b * dt)  
+        self.q_i = qm(self.q_i, dq)  # Update quaternion
 
-        print("de", de)
-        print("dp", dp)
-        return self.P_i, self.e
+        return self.P_i, self.q_i
 
 
 v_b = np.array([0.0, 0.0, 0.0], dtype=Num)
 omega_b = np.array([0.0, 0.0, 0.0], dtype=Num)
-dt = np.float32(0.02)
+dt = np.float64(0.02)
 
 
 async def broadcaster(ws):
     # TODO: move this outside of the broadcaster function
-    t = np.float32(0.0)  
+    t = np.float64(0.0)  
     P_i = np.array([0.0, 0.0, 0.0], dtype=Num)  # Initial position in inertial frame
-    e = np.array([0.0, 0.0, 0.0], dtype=Num)  # Initial Euler angles (phi, theta, psi)
+    q = e_to_q(np.zeros(3, dtype=Num))  # Initial orientation as quaternion
 
-    drone = Drone(P_i_0=P_i, e_0=e)
+    drone = KinematicObject(P_i_0=P_i, q_i_0=e_to_q(np.zeros(3, dtype=Num)))
 
     while True:
-        P_i, e = drone.update(v_b, omega_b, dt)
+        P_i, q = drone.update(v_b, omega_b, dt)
 
         state = {
             "time": t.astype(float),  
             "p_i": P_i.tolist(),  
-            "e": e.tolist(),  
+            "q": q.tolist(),  
             "v_b": v_b.tolist(),  
             "omega_b": omega_b.tolist(),
         }

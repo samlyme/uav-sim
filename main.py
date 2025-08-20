01a3747accbe425c03ed4a826475689237f57369
samlyme <sam@samly.me>
2025-08-20 14:07:33 -0700

    bugged idk might switch to quaternions lol

--- main.py
+++ main.py
@@ -9,66 +9,100 @@ import termios
 Num = np.float32
 
 LINEAR_SPEED = 1
-ANGULAR_SPEED = np.pi / 4  # radians per second
+ANGULAR_SPEED = np.pi / 4
 
 
-def R_b_v(e: np.ndarray) -> np.ndarray: # Rotation matrix from body frame to inertial frame
-    theta, phi, eta = e[1], e[0], e[2] 
+def R_b_v(e: np.ndarray) -> np.ndarray:
     """
-    Rotation matrix from body frame to inertial frame given roll (phi), pitch (theta), and yaw (eta).
+    Rotation matrix from body frame to inertial frame using Euler angles.
     """
-    R = np.array([
-        [ np.cos(theta) * np.cos(eta), np.cos(theta) * np.sin(eta), -np.sin(theta) ],
-        [ -np.cos(phi) * np.sin(eta) + np.sin(phi) * np.sin(theta) * np.cos(eta), np.cos(phi) * np.cos(eta) + np.sin(phi) * np.sin(theta) * np.sin(eta), np.sin(phi) * np.cos(theta)],
-        [ np.sin(phi) * np.sin(eta) + np.cos(phi) * np.sin(theta) * np.cos(eta), -np.sin(phi) * np.cos(eta) + np.cos(phi) * np.sin(theta) * np.sin(eta), np.cos(phi) * np.cos(theta)],
-    ], dtype=Num)
+    phi, theta, psi = e[0], e[1], e[2]  
+    R = np.array(
+        [
+            [
+                np.cos(theta) * np.cos(psi),
+                np.sin(theta) * np.sin(phi) * np.cos(psi) - np.cos(phi) * np.sin(psi),
+                np.cos(phi) * np.sin(theta) * np.cos(psi) + np.sin(phi) * np.sin(psi),
+            ],
+            [
+                np.cos(theta) * np.sin(psi),
+                np.sin(phi) * np.sin(theta) * np.sin(psi) + np.cos(phi) * np.cos(psi),
+                np.cos(phi) * np.sin(theta) * np.sin(psi) - np.sin(phi) * np.cos(psi),
+            ],
+            [
+                -np.sin(theta),
+                np.sin(phi) * np.cos(theta),
+                np.cos(phi) * np.cos(theta),
+            ]
+        ],
+        dtype=Num,
+    )
     return R
 
-# TODO: make this function mutate np arrays instead of returning new ones
-# This will improve performance by avoiding unnecessary memory allocation and copying.
-def update_state(
-    P_i_t0: np.ndarray,  # Position in inertial frame (pn, pe, -h)
-    e_t0: np.ndarray,    # Euler angles (phi, theta, eta)
-    v_b: np.ndarray,  # Velocity in body frame (u, v, w)
-    omega_b: np.ndarray,  # Angular velocity in body frame (p, q, r)
-    dt: float = 0.0,  # Time in seconds
-):
-    R = R_b_v(e_t0)  # Rotation matrix from body to inertial frame
-    P_i = P_i_t0 + R @ v_b * dt  # Position in inertial frame at time t
-    e = e_t0 + omega_b * dt  # Euler angles at time t
 
-    return P_i, e
+def J_b_v(e: np.ndarray) -> np.ndarray:
+    """
+    Jacobian of the Euler angles with respect to the angular velocity in body frame.
+    This is a 3x3 matrix that relates the angular velocity in body frame to the time derivative of the Euler angles.
+    """
+    phi, theta, psi = e[0], e[1], e[2]  # noqa: F841
+    return np.array(
+        [
+            [1, np.sin(phi) * np.tan(theta), np.cos(phi) * np.tan(theta)],
+            [0, np.cos(phi), -np.sin(phi)],
+            [0, np.sin(phi) / np.cos(theta), np.cos(phi) / np.cos(theta)],
+        ],
+        dtype=Num,
+    )
 
 
-t = 0.0
-dt = 0.02
-P_i = np.array([0.0, 0.0, 0.0], dtype=Num)  # Initial position in inertial frame
-e = np.array([0.0, 0.0, 0.0], dtype=Num)  # Initial Euler angles (phi, theta, eta)
-v_b = np.array([0.0, 0.0, 0.0], dtype=Num)  # Initial velocity in body frame (u, v, w)
-omega_b = np.array([0.0, 0.0, 0.0], dtype=Num)  # Initial angular velocity in body frame (p, q, r
+class Drone:
+    def __init__(self, P_i_0: np.ndarray, e_0: np.ndarray):
+        self.P_i = P_i_0  # Position in inertial frame (pn, pe, -h)
+        self.e = e_0  # Euler angles (phi, theta, eta)
+
+    def update(self, v_b: np.ndarray, omega_b: np.ndarray, dt: Num):
+        J = J_b_v(self.e)
+        de = J.T @ omega_b
+        self.e = self.e + de * dt
+
+        R = R_b_v(self.e)
+        dp = R @ v_b
+        self.P_i = self.P_i + dp * dt
+
+        print("de", de)
+        print("dp", dp)
+        return self.P_i, self.e
+
+
+v_b = np.array([0.0, 0.0, 0.0], dtype=Num)
+omega_b = np.array([0.0, 0.0, 0.0], dtype=Num)
+dt = np.float32(0.02)
+
 
 async def broadcaster(ws):
-    
+    # TODO: move this outside of the broadcaster function
+    t = np.float32(0.0)  
+    P_i = np.array([0.0, 0.0, 0.0], dtype=Num)  # Initial position in inertial frame
+    e = np.array([0.0, 0.0, 0.0], dtype=Num)  # Initial Euler angles (phi, theta, psi)
+
+    drone = Drone(P_i_0=P_i, e_0=e)
+
     while True:
-        P_i, e = update_state(
-            P_i_t0=P_i,
-            e_t0=e,
-            v_b=v_b,
-            omega_b=omega_b,
-            dt=dt
-        )
+        P_i, e = drone.update(v_b, omega_b, dt)
 
         state = {
-            "time": t,
-            "p_i": P_i.tolist(),  # Convert numpy array to list for JSON serialization
-            "e": e.tolist(),  # Convert numpy array to list for JSON serialization
-            "v_b": v_b.tolist(),  # Convert numpy array to list for JSON serialization
-            "omega_b": omega_b.tolist()  # Convert numpy array to list for JSON serialization
+            "time": t.astype(float),  
+            "p_i": P_i.tolist(),  
+            "e": e.tolist(),  
+            "v_b": v_b.tolist(),  
+            "omega_b": omega_b.tolist(),
         }
 
         await ws.send(json.dumps(state))
-        await asyncio.sleep(dt)
+        await asyncio.sleep(dt.astype(float))
         t += dt
+
 
 def getch():
     fd = sys.stdin.fileno()
@@ -80,46 +114,108 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old)
     return ch
 
+
 async def key_reader():
     loop = asyncio.get_running_loop()
     print("CONTROL KEYS: W/S = fwd/back, A/D = left/right, E/Q = up/down")
-    print("              J/L = yaw left/right, I/K = pitch up/down, U/O = roll left/right")
+    print(
+        "              J/L = yaw left/right, I/K = pitch up/down, U/O = roll left/right"
+    )
     print("Press 'x' to exit.")
 
     while True:
         ch = await loop.run_in_executor(None, getch)
-        if ch == 'w':
-            print("Moving forward")
-        elif ch == 's':
-            print("Moving backward")
-        elif ch == 'a':
-            print("Turning left")
-        elif ch == 'd':
-            print("Turning right")
-        elif ch == 'e':
-            print("Moving up")
-        elif ch == 'q':
-            print("Moving down")
-        elif ch == 'j':
-            print("Yawing left")
-        elif ch == 'l':
-            print("Yawing right")
-        elif ch == 'i':
-            print("Pitching up")
-        elif ch == 'k':
-            print("Pitching down")
-        elif ch == 'u':
-            print("Rolling left")
-        elif ch == 'o':
-            print("Rolling right")
-        elif ch == 'x':
+        if ch == "w":
+            if v_b[0] == 0: 
+                v_b[0] = LINEAR_SPEED  # Move forward
+            else:
+                v_b[0] = 0
+        elif ch == "s":
+            if v_b[0] == 0:
+                v_b[0] = -LINEAR_SPEED  # Move backward
+            else:
+                v_b[0] = 0
+        elif ch == "a":
+            if v_b[1] == 0:
+                v_b[1] = -LINEAR_SPEED  # Move left
+            else:
+                v_b[1] = 0
+        elif ch == "d":
+            if v_b[1] == 0:
+                v_b[1] = LINEAR_SPEED  # Move right
+            else:
+                v_b[1] = 0
+        elif ch == "e":
+            if v_b[2] == 0:
+                v_b[2] = -LINEAR_SPEED  # Move up
+            else:
+                v_b[2] = 0
+        elif ch == "q":
+            if v_b[2] == 0:
+                v_b[2] = LINEAR_SPEED  # Move down
+            else:
+                v_b[2] = 0
+        elif ch == "j":
+            if omega_b[2] == 0:
+                omega_b[2] = -ANGULAR_SPEED  # Yaw left
+            else:
+                omega_b[2] = 0
+        elif ch == "l":
+            if omega_b[2] == 0:
+                omega_b[2] = ANGULAR_SPEED  # Yaw right
+            else:
+                omega_b[2] = 0
+        elif ch == "i":
+            if omega_b[1] == 0:
+                omega_b[1] = ANGULAR_SPEED
+            else:
+                omega_b[1] = 0
+        elif ch == "k":
+            if omega_b[1] == 0:
+                omega_b[1] = -ANGULAR_SPEED
+            else:
+                omega_b[1] = 0
+        elif ch == "u":
+            if omega_b[0] == 0:
+                omega_b[0] = -ANGULAR_SPEED
+            else:
+                omega_b[0] = 0 
+        elif ch == "o":
+            if omega_b[0] == 0:
+                omega_b[0] = ANGULAR_SPEED
+            else:
+                omega_b[0] = 0 
+        elif ch == "x":
             print("Exiting...")
+            sys.exit(0)
             break
 
+
 async def main():
+    # print("yaw test")
+    # for i in range(8):
+    #     e = np.array([0, 0, i * np.pi / 4], dtype=Num)  
+        
+    #     print(R_b_v(e) @ np.array([1, 0, 0], dtype=Num))  
+
+    # print("pitch test")
+    # for i in range(8):
+    #     e = np.array([0, i * np.pi / 4, 0], dtype=Num)  
+        
+    #     print(R_b_v(e) @ np.array([1, 0, 0], dtype=Num))  
+
+    # print("roll test")
+    # for i in range(8):
+    #     e = np.array([i * np.pi / 4, 0, 0], dtype=Num)  
+        
+    #     print(R_b_v(e) @ np.array([1, 0, 0], dtype=Num))  
+    print("Starting key reader...")
+    asyncio.create_task(key_reader())
+
     print("Starting WebSocket server on ws://localhost:8765")
     async with websockets.serve(broadcaster, "0.0.0.0", 8765):
         await asyncio.Future()  # run forever
+
 
 if __name__ == "__main__":
     asyncio.run(main())
